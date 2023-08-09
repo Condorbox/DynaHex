e0935d6f52f03eeb3a69d09a477bc8e0c5a56c28
Condorbox <minecraft13122001@gmail.com>
2023-08-09 18:59:32 +0200

    Vector3 methods

--- include/dynaHex/core.h
+++ include/dynaHex/core.h
@@ -1,3 +1,5 @@
+#include <cmath>
+
 #ifndef DYNAHEX_CORE_H
 #define DYNAHEX_CORE_H
 
@@ -10,20 +12,114 @@ namespace DynaHex {
         real y;
         real z;
 
-        Vector3() :  x(0), y(0), z(0) {}
+        Vector3() :  x(0), y(0), z(0), pad(0){}
 
-        Vector3(const real x, const real y, const real z) : x(x), y(y), z(z) {}
+        Vector3(const real x, const real y, const real z) : x(x), y(y), z(z), pad(0) {}
 
-        /** Flips all the components of the vector. */
-        void invert()
-        {
+        real operator[](unsigned i) const {
+            if (i == 0) return x;
+            else if (i == 1) return y;
+            return z;
+        }
+
+        real& operator[](unsigned i) {
+            if (i == 0) return x;
+            else if (i == 1) return y;
+            return z;
+        }
+
+        Vector3 operator+(const Vector3& v) const {
+            return {x+v.x, y+v.y, z+v.z}; //Braced initializer
+        }
+
+        Vector3 operator-(const Vector3& v) const {
+            return {x-v.x, y-v.y, z-v.z};
+        }
+
+        Vector3 operator*(const real value) const {
+            return {x*value, y*value, z*value};
+        }
+
+        //Calculates and returns the scalar product
+        real operator *(const Vector3 &vector) const {
+            return x*vector.x + y*vector.y + z*vector.z;
+        }
+
+        //Calculates and returns the vector product of this vector with the given vector
+        Vector3 operator%(const Vector3 &vector) const {
+            return {y * vector.z - z * vector.y,z * vector.x - x * vector.z,x * vector.y - y * vector.x};
+        }
+
+        void operator+=(const Vector3& v) {
+            x += v.x;
+            y += v.y;
+            z += v.z;
+        }
+
+        void operator-=(const Vector3& v) {
+            x -= v.x;
+            y -= v.y;
+            z -= v.z;
+        }
+
+        void operator*=(const real value) {
+            x *= value;
+            y *= value;
+            z *= value;
+        }
+
+        // Updates this vector to be the vector product of its current value and the given vector
+        void operator %=(const Vector3 &vector) {
+            *this = vectorProduct(vector);
+        }
+
+        void invert() {
             x = -x;
             y = -y;
-            x = -z;
+            z = -z;
+        }
+
+        [[nodiscard]] real magnitude() const {
+            return std::sqrt(x*x+y*y+z*z);
+        }
+
+        [[nodiscard]] real squareMagnitude() const {
+            return x*x+y*y+z*z;
+        }
+
+        void normalize() {
+            real l = magnitude();
+            if (l > 0) {
+                (*this) *= ((real)1)/l;
+            }
+        }
+
+        void addScaledVector(const Vector3& vector, const real scale) {
+            x += vector.x * scale;
+            y += vector.y * scale;
+            z += vector.z * scale;
+        }
+
+        [[nodiscard]] Vector3 componentProduct(const Vector3 &vector) const {
+            return {x * vector.x, y * vector.y, z * vector.z};
+        }
+
+        void componentProductUpdate(const Vector3 &vector) {
+            x *= vector.x;
+            y *= vector.y;
+            z *= vector.z;
+        }
+
+        [[nodiscard]] real scalarProduct(const Vector3 &vector) const {
+            return x*vector.x + y*vector.y + z*vector.z;
+        }
+
+        [[nodiscard]] Vector3 vectorProduct(const Vector3 &vector) const {
+            return {y*vector.z - z*vector.y, z*vector.x - x*vector.z, x*vector.y - y*vector.x};
         }
 
     private:
-        /** Padding to ensure 4 word alignment. */
+        /** Padding to ensure 4 word alignment.  we can remove it safely*/
         real pad;
 
     };
--- include/dynaHex/precision.h
+++ include/dynaHex/precision.h
@@ -1,3 +1,7 @@
+#ifndef DYNAHEX_PRECISION_H
+#define DYNAHEX_PRECISION_H
+
+
 namespace DynaHex {
     /** Defines a real number precision. DynaHex can be compiled in
     * single- or double-precision versions. By default, single
@@ -5,3 +9,5 @@ namespace DynaHex {
 
     typedef float real;
 }
+
+#endif

55e31f1fef898d304decce64c66b80b0fa09296b
Condorbox <minecraft13122001@gmail.com>
2023-08-17 01:46:34 +0200

    core.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
+
 #include <iostream>
-#include <random>
-#include "scr/demos/Particles/Ballistic/BallisticDemo.h"
 
-using namespace dynahex;
-
-int main() {
-    std::cout << "Hello World" << std::endl;
-
-    unsigned long timesToDisplay = 100;
-
-    auto* app = new BallisticDemo();
-
-    app->changeAmmoType(3);
-
-    for (unsigned long i = 0; i < timesToDisplay; i++) {
-        app->update();
-        app->display();
-        std::cout << "___________________________________________" << std::endl;
-    }
-
+int main()
+{
+    std::cout << "Hello World!";
     return 0;
 }

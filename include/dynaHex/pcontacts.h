35b338bae67d7331660765ef5e3b90cc42b0874f
Condorbox <minecraft13122001@gmail.com>
2023-09-02 12:14:09 +0900

    Hard Constraints Collisions

---
+++ include/dynaHex/pcontacts.h
@@ -0,0 +1,114 @@
+#ifndef DYNAHEX_PCONTACTS_H
+#define DYNAHEX_PCONTACTS_H
+
+#include "particle.h"
+
+namespace dynahex {
+    /**
+    * A contact represents two objects in contact (in this case
+    * ParticleContact representing two particles). Resolving a
+    * contact removes their interpenetration, and applies sufficient
+    * impulse to keep them apart. Colliding bodies may also rebound.
+    *
+    * The contact has no callable functions, it just holds the
+    * contact details. To resolve a set of contacts, use the particle
+    * contact resolver class.
+    */
+
+    class ParticleContact {
+    public:
+        /**
+        * Holds the particles that are involved in the contact. The
+        * second of these can be NULL for contacts with the scenery.
+        */
+        Particle *particle[2];
+        /**
+        * Holds the normal restitution coefficient at the contact.
+        */
+        real restitution;
+        /**
+        * Holds the direction of the contact in world coordinates.
+        * (from the first object's perspective)
+        */
+        Vector3 contactNormal;
+        /**
+        * Holds the depth of penetration at the contact.
+        */
+        real penetration;
+        /**
+         * Holds the amount each particle is moved by during interpenetration resolution.
+         */
+        Vector3 particleMovement[2];
+    public:
+        /**
+        * Resolves this contact for both velocity and interpenetration.
+        */
+        void resolve(real duration);
+
+        /**
+        * Calculates the separating velocity at this contact.
+        */
+        [[nodiscard]] real calculateSeparatingVelocity() const;
+
+    private:
+        /**
+        * Handles the impulse calculations for this collision.
+        */
+        void resolveVelocity(real duration);
+
+        /**
+         * Handles the interpenetration resolution for this contact.
+         */
+        void resolveInterpenetration(real duration);
+    };
+
+    /**
+    * The contact resolution routine for particle contacts. One
+    * resolver instance can be shared for the entire simulation.
+    */
+    class ParticleContactResolver {
+    protected:
+        /**
+        * Holds the number of iterations allowed.
+        */
+        unsigned iterations;
+        /**
+        * This is a performance tracking value; we keep a record
+        * of the actual number of iterations used.
+        */
+        unsigned iterationsUsed;
+    public:
+        /**
+        * Creates a new contact resolver.
+        */
+        explicit ParticleContactResolver(unsigned iterations);
+        /**
+        * Sets the number of iterations that can be used.
+        */
+        void setIterations(unsigned iterations);
+        /**
+        * Resolves a set of particle contacts for both penetration
+        * and velocity.
+        */
+        void resolveContacts(ParticleContact *contactArray, unsigned numContacts, real duration);
+    };
+
+    /**
+     * This is the basic polymorphic interface for contact generators
+     * applying to particles.
+     */
+    class ParticleContactGenerator
+    {
+    public:
+        /**
+         * Fills the given contact structure with the generated
+         * contact. The contact pointer should point to the first
+         * available contact in a contact array, where limit is the
+         * maximum number of contacts in the array that can be written
+         * to. The method returns the number of contacts that have
+         * been written.
+         */
+        virtual unsigned addContact(ParticleContact *contact, unsigned limit) const = 0;
+    };
+}
+#endif //DYNAHEX_PCONTACTS_H
---
+++ include/dynaHex/plinks.h
@@ -0,0 +1,83 @@
+
+#ifndef DYNAHEX_PLINKS_H
+#define DYNAHEX_PLINKS_H
+
+#include "pcontacts.h"
+
+namespace dynahex {
+    /**
+    * Links connect two particles together, generating a contact if
+    * they violate the constraints of their link. It is used as a
+    * base class for cables and rods, and could be used as a base
+    * class for springs with a limit to their extension.
+    */
+    class ParticleLink : ParticleContactGenerator {
+    public:
+        /**
+        * Holds the pair of particles that are connected by this link.
+        */
+        Particle *particle[2];
+    protected:
+        /**
+        * Returns the current length of the link.
+        */
+        real currentLength() const;
+
+    public:
+        /**
+        * Generates the contacts to keep this link from being
+        * violated. This class can only ever generate a single
+        * contact, so the pointer can be a pointer to a single
+        * element, the limit parameter is assumed to be at least 1
+        * (0 isn’t valid), and the return value is 0 if the
+                * cable wasn’t overextended, or 1 if a contact was needed.
+        *
+        * NB: This method is declared in the same way (as pure
+        * virtual) in the parent class, but is replicated here for
+        * documentation purposes.
+        */
+        unsigned addContact(ParticleContact *contact, unsigned limit) const override = 0;
+    };
+
+    /**
+    * Cables link a pair of particles, generating a contact if they
+    * stray too far apart.
+    */
+    class ParticleCable : public ParticleLink {
+    public:
+        /**
+        * Holds the maximum length of the cable.
+        */
+        real maxLength;
+        /**
+        * Holds the restitution (bounciness) of the cable.
+        */
+        real restitution;
+
+    public:
+        /**
+        * Fills the given contact structure with the contact needed
+        * to keep the cable from overextending.
+        */
+        unsigned addContact(ParticleContact *contact, unsigned limit) const override;
+    };
+
+    /**
+    * Rods link a pair of particles, generating a contact if they
+    * stray too far apart or too close.
+    */
+    class ParticleRod : public ParticleLink {
+    public:
+        /**
+        * Holds the length of the rod.
+        */
+        real length;
+    public:
+        /**
+        * Fills the given contact structure with the contact needed
+        * to keep the rod from extending or compressing.
+        */
+        unsigned addContact(ParticleContact *contact, unsigned limit) const override;
+    };
+}
+#endif //DYNAHEX_PLINKS_H
